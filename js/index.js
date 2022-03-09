f19955163c488d4ec5e37af5ae7ffd701f6f75f0
ZakyUp <andresjr195@hotmail.com>
2022-03-09 14:02:48 -0500

    Cyberpunk2077

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// function fix_scroll() {
+//     let s = $(window).scrollTop();
+//     let fixedTitle = $('#navbar');
+//     fixedTitle.css('position','absolute');
+//     fixedTitle.css('top',s + 'px');
+// }fix_scroll();
+
+// $(window).on('scroll',fix_scroll);
+
